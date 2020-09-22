56975a24ea04d06ba93d382158531ad6a8581fe5
Michael P Garrity <mike@mikegslptop2495.lan>
2020-09-22 12:00:10 -0400

    Updates

---
+++ precache-manifest.6ad0e62b9156678f110111e18edcdee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c67cbb64a96c7e3ee7d3f0e0dcabfc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04f01e11212149d4311b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c10f14cb54eeb7805f5",
+    "url": "/static/js/2.ac9a6ce4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ac9a6ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f01e11212149d4311b",
+    "url": "/static/js/main.42112772.chunk.js"
+  },
+  {
+    "revision": "dd59932bee74f12212ae",
+    "url": "/static/js/runtime-main.26d1f92d.js"
+  }
+]);
